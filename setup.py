8e323123058632d6382b6953d0d7d900a55799ad
Diego Souza <diego@wespa.com.br>
2019-09-16 23:38:16 -0300

    Updating library version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pywup",
-    version="0.0.9",
+    version="0.0.13",
     author="Diego Souza",
     author_email="contact@wespa.com.br",
     description="A small set of tools",
