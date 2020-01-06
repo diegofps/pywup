6c6395482b33b3ece6b4ba20c251f0a95a791d2b
Diego Souza <diego@wespa.com.br>
2020-01-06 15:06:56 -0300

    Finishing env refactoring

--- pywup/cmdtools/env.py
+++ pywup/cmdtools/env.py
@@ -1,5 +1,6 @@
 from pywup.services.general import parse_env, get_image_name, get_container_name, get_open_cmd, parse_image_name, update_state, get_export_filepath
 from pywup.services.system import error, abort, WupError, run, Args, Route, quote
+from pywup.services.env import Env
 from pywup.services import conf
 
 import sys
@@ -8,168 +9,63 @@ import os
 
 def do_set(args):
     if args.has_parameter():
-        name, filepath = parse_image_name(args.pop_parameter())
+        Env().set(args.pop_parameter())
     else:
-        name, filepath = "", ""
+        Env().set("")
     
-    conf.set("wup.env_name", name, scope="global")
-    conf.set("wup.env_filepath", filepath, scope="global")
     update_state()
 
 
 def do_build(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    variables, templates, bashrc = parse_env(filepath, "build")
-    cont_name = get_container_name(name)
-    
-    volumes = " -v " + " -v ".join(templates["VOLUMES"]) if templates["VOLUMES"] else None
-    expose = " --expose=" + " --expose=".join(variables["EXPOSE"].split(",")) if variables["EXPOSE"] else None
-    mapPorts = " -p " + " -p".join(variables["MAP_PORTS"].split(",")) if variables["MAP_PORTS"] else None
-    create_start = ["echo " + quote("".join(templates["START"] + ["bash\n", "exit\n"])) + " > /start.sh\n", "chmod +x /start.sh\n"]
-
-    cmds = bashrc + create_start + templates["BUILD"]
-    base = variables["BASE"]
-
-    rmCmd = "docker rm %s 2> /dev/null" % cont_name
-    print(rmCmd)
-    run(rmCmd)
-
-    createCmd = "docker run -i --entrypoint /bin/bash --name " + cont_name
-
-    if volumes:
-        createCmd += volumes
-    
-    if expose:
-        createCmd += expose
-        
-    if mapPorts:
-        createCmd += mapPorts
-    
-    #a = quote("nohup /start.sh & bash ; exit")
-    b = quote("bash --init-file <(cat /start.sh 2> /dev/null || echo \"bash ; exit\")")
-    createCmd += " " + base + " -c " + b
-    
-    print(createCmd)
-    run(createCmd, write=cmds)
+    Env().build()
 
 
 def do_open(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    variables, templates, bashrc = parse_env(filepath)
-    cont_name = get_container_name(name)
-    cmds = bashrc + templates["OPEN"]
-
-    print("Starting container...")
-    run("docker start " + cont_name)
-
-    print("Connecting to terminal...")
-    run(get_open_cmd(cont_name, cmds, True))
+    Env().open()
 
 
 def do_commit(args):
-    name = conf.get("wup.env_name", scope="global")
-
-    container_name = get_container_name(name)
-    img_name = get_image_name(name)
-
-    print("Removing any existing image with this name")
-    run("docker rmi {} 2> /dev/null".format(img_name))
-
-    print("Creating the new image")
-    run("docker commit {} {}".format(container_name, img_name))
+    Env().commit()
 
 
 def do_launch(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    variables, templates, bashrc = parse_env(filepath, "launch")
-    cont_name = get_container_name(name)
-    cmds = bashrc + templates["LAUNCH"] + ["\nexit\n"]
-
-    print("Starting container...")
-    run("docker start " + cont_name)
-
-    print("Connecting to terminal...")
-    run(get_open_cmd(cont_name, cmds))
+    Env().launch()
 
 
 def do_exec(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    cmd = args.pop_parameter()
-    cont_name = get_container_name(name)
-    variables, templates, bashrc = parse_env(filepath, "exec")
-
-    cmds = bashrc + templates["EXEC"] + [cmd, "\nexit\n"]
-
-    print("Starting container...")
-    run("docker start " + cont_name)
-
-    print("Connecting to terminal...")
-    run(get_open_cmd(cont_name, cmds))
+    Env().exec([args.pop_parameter])
 
 
 def do_export(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    img_name = get_image_name(name)
-    filepath = get_export_filepath(name)
-
-    print("Exporting image commit for", img_name, "as", filepath)
-    run("docker save {} | gzip > {}".format(img_name, filepath))
+    Env().export()
 
 
 def do_import(args):
-    filepath = args.pop_parameter()
+    Env().load(args.pop_parameter())
 
-    print("Importing image...")
-    run(["zcat {}".format(filepath), "docker load -q"])
+
+def do_run(args):
+    Env().run(" ".join(args.all()))
 
 
 def do_new(args):
-    name = conf.get("wup.env_name", scope="global")
-    filepath = conf.get("wup.env_filepath", scope="global")
-
-    variables, templates, bashrc = parse_env(filepath, "new")
-    img_name = get_image_name(name)
-    cont_name = get_container_name(name)
-    volumes = "-v " + " -v ".join(templates["VOLUMES"]) if templates["VOLUMES"] else ""
-    cmds = bashrc + templates["NEW"]
-    
-    print("Removing previous container...")
-    run("docker rm %s 2> /dev/null" % cont_name)
-
-    print("Creating container from image...")
-    run("docker run -i --name {} {} {}".format(cont_name, volumes, img_name), write=cmds)
+    Env().new()
 
 
 def do_rm(args):
-    name = conf.get("wup.env_name", scope="global")
-    cont_name = get_container_name(name)
+    Env().rm()
 
-    run("docker kill " + cont_name)
-    run("docker rm " + cont_name)
 
 def do_rmi(args):
-    name = conf.get("wup.env_name", scope="global")
-    img_name = get_image_name(name)
-
-    run("docker rmi " + img_name)
+    Env().rmi()
 
 
 def do_ls(args):
-    run("docker ps -a -f \"name=wcont__*\"")
+    Env().ls()
 
 
 def do_lsi(args):
-    run("docker image ls \"wimg*\"")
+    Env().lsi()
 
 
 def main(args):
@@ -180,6 +76,7 @@ def main(args):
     r.map("open", do_open, "Opens the environment")
     r.map("launch", do_launch, "Executes the commands in @LAUNCH@ from inside the environment")
     r.map("exec", do_exec, "Executes a custom command inside the environment")
+    r.map("run", do_run, "Executes the command define with RUN inside the environment with the parameters given")
     r.map("ls", do_ls, "Lists all environments")
     r.map("lsi", do_lsi, "Lists all environment images")
     r.map("rm", do_rm, "Remove an environment")
--- pywup/services/env.py
+++ pywup/services/env.py
@@ -1,4 +1,4 @@
-from pywup.services.general import parse_env, get_container_name, get_image_name, get_open_cmd, parse_env, parse_image_name
+from pywup.services.general import parse_env, get_container_name, get_image_name, get_open_cmd, parse_env, parse_image_name, get_export_filepath
 from pywup.services.system import run, error, quote
 from pywup.services import conf
 
@@ -7,40 +7,65 @@ import os
 
 class Env:
 
-    def __init__(self, filepath=None):
-        if filepath:
-            self.set(filepath)
+    def __init__(self):
         self.reload()
     
 
     def reload(self):
-        self.name = conf.get("wup.env_name", scope="global")
-        self.filepath = conf.get("wup.env_filepath", scope="global")
-        self.cont_name = get_container_name(self.name)
-        self.img_name = get_image_name(self.name)
-        self.variables, self.templates, self.bashrc = parse_env(self.filepath)
+        try:
+            self.name = conf.get("wup.env_name", scope="global")
+            self.filepath = conf.get("wup.env_filepath", scope="global")
+
+            if self.filepath:
+                self.variables, self.templates, self.bashrc = parse_env(self.filepath)
+                self.cont_name = get_container_name(self.name)
+                self.img_name = get_image_name(self.name)
+            
+        except:
+            self.name = ""
+            self.filepath = ""
+            self.cont_name = None
+            self.img_name = None
+            self.variables, self.templates, self.bashrc = None, None, None
 
 
     def set(self, filepath):
-        if not filepath:
-            error("Invalid filepath")
-        else:
+        if filepath:
             name, filepath = parse_image_name(filepath)
+        else:
+            name,filepath = "", ""
 
         conf.set("wup.env_name", name, scope="global")
         conf.set("wup.env_filepath", filepath, scope="global")
 
 
+    def ip(self):
+        cmd = "docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' " + self.cont_name
+        status, rows = run(cmd, read=True)
+
+        if status != 0 or not rows:
+            error("Command error")
+        
+        return rows[0].strip()
+    
+
     def is_running(self):
         cmd = "docker inspect -f '{{.State.Running}}' " + self.cont_name
         status, rows = run(cmd, read=True)
-
-        #import pdb; pdb.set_trace()
 
         if status != 0 or not rows:
             error("Command error")
         
         return rows[0] == "true\n"
+
+
+    def has_img(self):
+        status, rows = run("docker image ls \"{}\"".format(self.img_name), read=True)
+
+        if status != 0:
+            error("Command error")
+        
+        return len(rows) == 2
 
 
     def build(self):
@@ -91,7 +116,7 @@ class Env:
 
 
     def run(self, params):
-        cmd = [self.variables["RUN"] + params]
+        cmd = [self.variables["RUN"] + " " + params]
         self.exec(cmd)
 
 
@@ -119,3 +144,39 @@ class Env:
 
     def lsi(self):
         run("docker image ls \"wimg*\"")
+
+
+    def export(self):
+        filepath = get_export_filepath(self.name)
+
+        print("Exporting image commit for", self.img_name, "as", filepath)
+        run("docker save {} | gzip > {}".format(self.img_name, filepath))
+
+
+    def load(self, filepath):
+        print("Importing image...")
+        run(["zcat " + filepath, "docker load -q"])
+
+
+    def new(self):
+        if not self.has_img():
+            error("Image not found")
+
+        self.rm()
+
+        createCmd = "docker run -i --name " + self.cont_name
+
+        if self.templates["VOLUMES"]:
+            createCmd += " -v " + " -v ".join(self.templates["VOLUMES"])
+        
+        if self.variables["EXPOSE"]:
+            createCmd += " --expose=" + " --expose=".join(self.variables["EXPOSE"].split(","))
+
+        if self.variables["MAP_PORTS"]:
+            createCmd += " -p " + " -p".join(self.variables["MAP_PORTS"].split(","))
+
+        createCmd += " " + self.img_name
+        print(createCmd)
+
+        cmds = self.bashrc + self.templates["NEW"]
+        run(createCmd, write=cmds)
