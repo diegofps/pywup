d1b3f8c74d40212ebe7b2c50a665f2c4eeb3267f
Diego Souza <diego@wespa.com.br>
2020-01-17 20:13:40 -0300

    Adding --q parameter to wup q

--- pywup/cmdtools/q.py
+++ pywup/cmdtools/q.py
@@ -1,5 +1,31 @@
+from pywup.services.system import error
+
 import os
 
 def main(args):
-    query = args.pop_parameter()
-    os.system("q -H -d \";\" \"" + query + "\" -O")
+    show_header = True
+    query = None
+
+    while args.has_next():
+        if args.has_cmd():
+            cmd = args.pop_cmd()
+
+            if cmd == "--q":
+                show_header = False
+
+            else:
+                error("Invalid parameter:", cmd)
+            
+        elif args.has_parameter():
+            query = args.pop_parameter()
+
+        else:
+            error("Unexpected input: ", args.pop_next())
+    
+    if query is None:
+        error("Missing query")
+    
+    if show_header:
+        os.system("q -H -d \";\" \"" + query + "\" -O")
+    else:
+        os.system("q -H -d \";\" \"" + query + "\"")
