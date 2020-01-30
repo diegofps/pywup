e30ec336a4812743d9cea5db9c838a591223c151
Diego Souza <diego@wespa.com.br>
2020-01-30 18:30:21 -0300

    Working wrap.py

--- wrap.py
+++ wrap.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from pywup.services.system import yprint
+from pywup.services.system import colors, yprint, wprint, rprint
+from collections import defaultdict
 from subprocess import Popen
 
+import hashlib
 import termios
 import string
 import select
@@ -83,7 +85,6 @@ def test1():
         cmd_chars = []
         
         while not finished:
-            #r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
             r, w, e = select.select([sys.stdin], [], [])
             
             d = os.read(sys.stdin.fileno(), 10240)
@@ -115,6 +116,302 @@ def test1():
     sys.exit(0)
 
 
+def aand(arr):
+    for x in arr:
+        if not x:
+            return False
+    return True
+
+
+def parse(master, outputs, secret_n):
+    o = os.read(master, 10240)
+
+    os.write(sys.stdout.fileno(), o)
+
+    if not o:
+        return True
+    
+    last = 0
+    i = 0
+
+    linebreak = ord('\r')
+    result = []
+
+    while i != len(o):
+        if o[i] == linebreak:
+            result.append(o[last:i+1])
+            last = i + 1
+        
+        i += 1
+    
+    result.append(o[last:i])
+
+    if outputs:
+        outputs[-1] = outputs[-1] + result[0]
+        outputs.extend(result[1:])
+    
+    else:
+        outputs.extend(result)
+
+    for row in result:
+        if secret_n in row:
+            return False
+    
+    return True
+
+
+class Wrapper:
+
+    def __init__(self, num):
+        import readline
+
+        self.command = "bash"
+        self.wup_env_open = b"/home/diego/Sources/pywup/pywup/wup.py env open\n"
+        self.masters = []
+        self.slaves = []
+        self.popens = []
+        
+        self.start_secret = b"f897473fkjree78wefkfw89743jurewjlkhwufwe89343f"
+        self.print_start_secret_cmd = b"echo '%s'\n" % self.start_secret
+        self.start_secret_n = self.start_secret + b"\r"
+
+        self.stop_secret  = b"odssd894gfmd9023milewjclkwmjiweofjweodwsdji324"
+        self.print_stop_secret_cmd = b"echo '%s'\n" % self.stop_secret
+        self.stop_secret_n = self.stop_secret + b"\r"
+
+        for _ in range(num):
+            master_fd, slave_fd = pty.openpty()
+            popen = Popen(shlex.shlex(self.command),
+                    preexec_fn=os.setsid,
+                    stdin=slave_fd,
+                    stdout=slave_fd,
+                    stderr=slave_fd,
+                    universal_newlines=True)
+            
+            self.popens.append(popen)
+            self.masters.append(master_fd)
+            self.slaves.append(slave_fd)
+        
+        for master in self.masters:
+            os.write(master, self.wup_env_open)
+    
+    def loop(self):
+        while True:
+            try:
+                sys.stdout.write(colors.WHITE)
+                cmd = input(">> ").strip()
+                sys.stdout.write(colors.RESET)
+
+                if cmd == "":
+                    continue
+
+                elif cmd == "exit":
+                    break
+                    
+                else:
+                    self.print_start_secret()
+                    self.execute(cmd)
+                    self.print_stop_secret()
+                    self.collect_outputs()
+                    self.display_outputs_3()
+            except EOFError:
+                print()
+                break
+    
+    def print_start_secret(self):
+        for master in self.masters:
+            os.write(master, self.print_start_secret_cmd)
+
+    def execute(self, cmd):
+        for master in self.masters:
+            os.write(master, cmd.encode() + b"\n")
+
+    def print_stop_secret(self):
+        for master in self.masters:
+            os.write(master, self.print_stop_secret_cmd)
+
+    def collect_outputs(self):
+        self.outputs = [[] for _ in self.masters]
+
+        starts = [1 for _ in self.masters]
+        stops  = [1 for _ in self.masters]
+
+        while sum(starts) != 0 or sum(stops) != 0:
+            r, w, e = select.select(self.masters, [], [])
+        
+            for i, master in enumerate(self.masters):
+                if master in r:
+                    found_start, found_stop = self.parse(master, self.outputs[i])
+
+                    if found_start:
+                        starts[i] = 0
+                    
+                    if found_stop:
+                        stops[i] = 0
+                    
+                    break
+
+    def display_outputs_0(self):
+        for i, data in enumerate(self.outputs):
+
+            print()
+            yprint("--- OUTPUT FOR %d ---" % i)
+
+            for j, row in enumerate(data):
+                os.write(sys.stdout.fileno(), b"\n[%d]\n" % j)
+                os.write(sys.stdout.fileno(), row)
+    
+    def display_outputs_1(self):
+        for i, data in enumerate(self.outputs):
+
+            print()
+            yprint("--- OUTPUT FOR %d WAS ---" % i)
+            state = 0
+
+            for j, row in enumerate(data):
+                if state == 0:
+                    if self.start_secret_n in row:
+                        state = 1
+                
+                elif state == 1:
+                    if self.stop_secret in row:
+                        break
+                    else:
+                        os.write(sys.stdout.fileno(), b"\n[%d]\n" % j)
+                        os.write(sys.stdout.fileno(), row)
+                
+                else:
+                    break
+        
+    def display_outputs_2(self, verbose=False):
+        for i, data in enumerate(self.outputs):
+
+            print()
+            yprint("--- OUTPUT FOR %d WAS ---" % i)
+            
+            start = None
+            stop = None
+
+            for j, row in enumerate(data):
+                if self.start_secret in row:
+                    start = j + 2
+                
+                if stop is None and self.stop_secret in row:
+                    stop = j
+
+            if start is None or stop is None:
+                for j, row in enumerate(data):
+                    if verbose:
+                        os.write(sys.stdout.fileno(), b"\n[%d]\n" % j)
+                        os.write(sys.stdout.fileno(), row)
+                    else:
+                        os.write(sys.stdout.fileno(), row)
+            
+            else:
+                for j in range(start, stop):
+                    row = data[j]
+
+                    if verbose:
+                        os.write(sys.stdout.fileno(), b"\n[%d]\n" % j)
+                        os.write(sys.stdout.fileno(), row)
+                    else:
+                        os.write(sys.stdout.fileno(), row)
+    
+    def display_outputs_3(self, verbose=False):
+        hash_to_names = defaultdict(list)
+        hash_to_outputs = {}
+
+        for i, data in enumerate(self.outputs):
+
+            #print()
+            #yprint("--- ORIGINAL OUTPUT FOR %s WAS ---" % str(i))
+
+            #for j, row in enumerate(data):
+            #    os.write(sys.stdout.fileno(), b"\n[%d]\n" % j)
+            #    os.write(sys.stdout.fileno(), row)
+            
+            start = None
+            stop = None
+
+            for j, row in enumerate(data):
+                if self.start_secret in row:
+                    start = j + 2
+                
+                if stop is None and self.stop_secret_n in row:
+                    stop = j - 1
+
+            #print("START", start, "STOP", stop)
+
+            if start is not None and stop is not None:
+                data = data[start:stop]
+            
+            m = hashlib.md5()
+            
+            for row in data:
+                m.update(row)
+            
+            key = m.digest()
+            name = str(i)
+
+            hash_to_names[key].append(name)
+            if not key in hash_to_outputs:
+                hash_to_outputs[key] = data
+
+        #rprint("------------------------------------------------------------")
+
+        for key, data in hash_to_outputs.items():
+            names = hash_to_names[key]
+
+            print()
+            yprint("--- OUTPUT FOR %s WAS ---" % ", ".join(names))
+
+            for row in data:
+                os.write(sys.stdout.fileno(), row)
+
+
+    def parse(self, master, outputs):
+        o = os.read(master, 10240)
+
+        #os.write(sys.stdout.fileno(), o)
+
+        if not o:
+            return False, False
+        
+        last = 0
+        i = 0
+
+        linebreak = ord('\n')
+        result = []
+
+        while i != len(o):
+            if o[i] == linebreak:
+                result.append(o[last:i+1])
+                last = i + 1
+            
+            i += 1
+        
+        result.append(o[last:i])
+
+        if outputs:
+            outputs[-1] = outputs[-1] + result[0]
+            outputs.extend(result[1:])
+        
+        else:
+            outputs.extend(result)
+
+        found_start = False
+        found_stop = False
+
+        for row in result:
+            if self.start_secret_n in row:
+                found_start = True
+            
+            if self.stop_secret_n in row:
+                found_stop = True
+        
+        return found_start, found_stop
+
+
 def test2():
     import readline
 
@@ -137,7 +434,7 @@ def test2():
             stderr=slave_fd2,
             universal_newlines=True)
 
-    secret = b"reilvtoapmbjnkkrehfnewuihnsdjhweurynewvktykrtjsncfjbvterx"
+    secret = b"reilvtoapmbjnkkrehfnewuihnsdjhweurynewvktykrt"
     secret2 = secret + b"\r"
     print_secret = b"echo '%s'\n" % secret
 
@@ -163,39 +460,25 @@ def test2():
         os.write(master_fd1, print_secret)
         os.write(master_fd2, print_secret)
 
-        outputs = [b"", b""]
-        todos = [1, 1]
+        outputs = [[], []]
+        todos = [True, True]
 
-        while sum(todos) != 0:
+        while aand(todos):
 
-            r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
+            r, w, e = select.select([master_fd1, master_fd2], [], [])
         
-            if sys.stdin in r:
-                d = os.read(sys.stdin.fileno(), 10240)
-            
-            elif master_fd1 in r:
-                
-                o = os.read(master_fd1, 10240)
-                #os.write(sys.stdout.fileno(), o)
-
-                if o:
-                    outputs[0] = outputs[0] + o
-                    if secret2 in outputs[0]:
-                        todos[0] = 0
+            if master_fd1 in r:
+                todos[0] = parse(master_fd1, outputs[0], secret2) and todos[0]
 
             elif master_fd2 in r:
-                o = os.read(master_fd2, 10240)
-                #os.write(sys.stdout.fileno(), o)
-                
-                if o:
-                    outputs[1] = outputs[1] + o
-                    if secret2 in outputs[1]:
-                        todos[1] = 0
+                todos[1] = parse(master_fd2, outputs[1], secret2) and todos[1]
         
         for i, data in enumerate(outputs):
             print()
             yprint("--- OUTPUT FOR %d WAS ---" % i)
-            os.write(sys.stdout.fileno(), data)
+            for i, row in enumerate(data):
+                os.write(sys.stdout.fileno(), b"%d" % i)
+                os.write(sys.stdout.fileno(), row)
 
 
 def test3():
@@ -259,5 +542,8 @@ def test3():
     TurtleShell().cmdloop()
 
 
-test2()
+def test4():
+    Wrapper(3).loop()
+
+test4()
   
