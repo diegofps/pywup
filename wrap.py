b0fa61659d5520522e25a1c120f00b1235e8d0fe
Diego Souza <diego@wespa.com.br>
2020-01-30 15:23:10 -0300

    Improving wrap.py

--- wrap.py
+++ wrap.py
@@ -2,98 +2,254 @@
 
 from subprocess import Popen
 
-import os
-import sys
-import select
 import termios
+import string
+import select
+import shlex
+import sys
 import tty
 import pty
-import shlex
+import os
 
-command = shlex.shlex("wup env open")
 
-old_tty = termios.tcgetattr(sys.stdin)
+def test0():
 
-try:
-    tty.setraw(sys.stdin.fileno())
+    command = "python3 /home/diego/Sources/pywup/pywup/wup.py env open"
 
     master_fd1, slave_fd1 = pty.openpty()
-    p1 = Popen(shlex.shlex("wup env open"),
-              preexec_fn=os.setsid,
-              stdin=slave_fd1,
-              stdout=slave_fd1,
-              stderr=slave_fd1,
-              universal_newlines=True)
-      
+    p1 = Popen(shlex.shlex(command),
+            preexec_fn=os.setsid,
+            stdin=slave_fd1,
+            stdout=slave_fd1,
+            stderr=slave_fd1,
+            universal_newlines=True)
+    
     master_fd2, slave_fd2 = pty.openpty()
-    p2 = Popen(shlex.shlex("wup env open"),
-              preexec_fn=os.setsid,
-              stdin=slave_fd2,
-              stdout=slave_fd2,
-              stderr=slave_fd2,
-              universal_newlines=True)
+    p2 = Popen(shlex.shlex(command),
+            preexec_fn=os.setsid,
+            stdin=slave_fd2,
+            stdout=slave_fd2,
+            stderr=slave_fd2,
+            universal_newlines=True)
 
-    buffer1 = []
-    buffer2 = []
+    while p1.poll() is None and p2.poll() is None:
+        r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
+        
+        if sys.stdin in r:
+            d = os.read(sys.stdin.fileno(), 10240)
+            os.write(master_fd1, d)
+            os.write(master_fd2, d)
+        
+        elif master_fd1 in r:
+            o = os.read(master_fd1, 10240)
+            if o:
+                os.write(sys.stdout.fileno(), o)
 
-    #os.write(master_fd1, "ls /\n".encode())
-    #os.write(master_fd2, "ls /\n".encode())
+        elif master_fd2 in r:
+            o = os.read(master_fd2, 10240)
+            if o:
+                os.write(sys.stdout.fileno(), o)
+  
+
+
+def test1():
+    command = "python3 /home/diego/Sources/pywup/pywup/wup.py env open"
+
+    old_tty = termios.tcgetattr(sys.stdin)
+
+    try:
+        tty.setraw(sys.stdin.fileno())
+
+        master_fd1, slave_fd1 = pty.openpty()
+        p1 = Popen(shlex.shlex(command),
+                preexec_fn=os.setsid,
+                stdin=slave_fd1,
+                stdout=slave_fd1,
+                stderr=slave_fd1,
+                universal_newlines=True)
+        
+        master_fd2, slave_fd2 = pty.openpty()
+        p2 = Popen(shlex.shlex(command),
+                preexec_fn=os.setsid,
+                stdin=slave_fd2,
+                stdout=slave_fd2,
+                stderr=slave_fd2,
+                universal_newlines=True)
+
+        os.write(sys.stdout.fileno(), ">> ".encode())
+        printable = string.printable.encode()
+        finished = False
+        cmd_chars = []
+        
+        while not finished:
+            #r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
+            r, w, e = select.select([sys.stdin], [], [])
+            
+            d = os.read(sys.stdin.fileno(), 10240)
+            
+            for x in range(len(d)):
+                c = d[x]
+                bc = d[x:x+1]
+
+                if bc in printable:
+                    cmd_chars.append(c)
+                    os.write(sys.stdout.fileno(), bc)
+
+                if c == ord('\r'):
+                    cmd = bytes(cmd_chars[:-1])
+                    cmd_chars.clear()
+
+                    if cmd == b"exit":
+                        os.write(sys.stdout.fileno(), b"\r\n")
+                        finished = True
+                        break
+                    else:
+                        os.write(sys.stdout.fileno(), b"\nUnknown command: " + cmd + b"\r")
+                    
+                    os.write(sys.stdout.fileno(), b"\n>> ")
+            
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
+
+    sys.exit(0)
+
+
+def test2():
+    import readline
+
+    command = "bash"
+
+    master_fd1, slave_fd1 = pty.openpty()
+    p1 = Popen(shlex.shlex(command),
+            preexec_fn=os.setsid,
+            stdin=slave_fd1,
+            stdout=slave_fd1,
+            stderr=slave_fd1,
+            universal_newlines=True)
     
-    os.write(sys.stdout.fileno(), ">> ".encode())
-    os.write(sys.stdout.fileno(), (str(ord("\r"))+"\n").encode())
-    
+    master_fd2, slave_fd2 = pty.openpty()
+    p2 = Popen(shlex.shlex(command),
+            preexec_fn=os.setsid,
+            stdin=slave_fd2,
+            stdout=slave_fd2,
+            stderr=slave_fd2,
+            universal_newlines=True)
+
+    secret = b"reilvtoapmbjnkkrehfnewuihnsdjhweurynewvktykrtjsncfjbvterx"
+    secret2 = secret + b"\r"
+    print_secret = b"echo '%s'\n" % secret
+
     while True:
-        #r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
-        r, w, e = select.select([sys.stdin], [], [])
-        
-        d = os.read(sys.stdin.fileno(), 10240)
-        last = 0
-        i = 0
-        
-        os.write(sys.stdout.fileno(), str(len(d)).encode())
-        os.write(sys.stdout.fileno(), d)
-        
-        if d[0] == ord("a"):
+        try:
+            cmd = input(">> ").strip()
+        except EOFError:
+            print()
+            cmd = "exit"
+
+        if cmd == "":
+            continue
+
+        if cmd == "exit":
             break
         
-        continue
-        #os.write(sys.stdout.fileno(), (str(d[0])+"\n").encode())
+        os.write(master_fd1, cmd.encode() + b"\n")
+        os.write(master_fd2, cmd.encode() + b"\n")
+
+        os.write(master_fd1, print_secret)
+        os.write(master_fd2, print_secret)
+
+        outputs = [b"", b""]
+        todos = [1, 1]
+
+        while sum(todos) != 0:
+
+            r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
         
-        for char in d:
-            if char == '\r':
-                os.write(sys.stdout.fileno(), d[last:i])
-                os.write(sys.stdout.fileno(), ">> ".encode())
-                last = i
+            if sys.stdin in r:
+                d = os.read(sys.stdin.fileno(), 10240)
             
-            elif char == 'a':
-                sys.exit(0)
+            elif master_fd1 in r:
+                o = os.read(master_fd1, 10240)
+
+                if o:
+                    outputs[0] = outputs[0] + o
+                    if secret2 in outputs[0]:
+                        todos[0] = 0
+
+            elif master_fd2 in r:
+                o = os.read(master_fd2, 10240)
+                
+                if o:
+                    outputs[1] = outputs[1] + o
+                    if secret2 in outputs[1]:
+                        todos[1] = 0
+        
+        for i, data in enumerate(outputs):
+            print()
+            print("--- OUTPUT FOR %d WAS ---" % i)
+            os.write(sys.stdout.fileno(), outputs[i])
+
+
+def test3():
+    import cmd, sys
+
+    class TurtleShell(cmd.Cmd):
+        intro = 'Welcome to the turtle shell.   Type help or ? to list commands.\n'
+        prompt = '(turtle) '
+        file = None
+
+        # ----- basic turtle commands -----
+        def do_forward(self, arg):
+            'Move the turtle forward by the specified distance:  FORWARD 10'
+            print('Move the turtle forward by the specified distance:  FORWARD 10')
             
-            i+=1
-        
-        #os.write(sys.stdout.fileno(), ("%d %d\n" % (last, i)).encode())
-        os.write(sys.stdout.fileno(), d[last:i])
-        
-finally:
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
+        def do_right(self, arg):
+            'Turn turtle right by given number of degrees:  RIGHT 20'
+            print('Turn turtle right by given number of degrees:  RIGHT 20')
+            
+        def do_left(self, arg):
+            'Turn turtle left by given number of degrees:  LEFT 90'
+            print('Turn turtle left by given number of degrees:  LEFT 90')
+            
+        def do_goto(self, arg):
+            'Move turtle to an absolute position with changing orientation.  GOTO 100 200'
+            print('Move turtle to an absolute position with changing orientation.  GOTO 100 200')
+            
+        def do_home(self, arg):
+            'Return turtle to the home position:  HOME'
+            print('Return turtle to the home position:  HOME')
+            
+        def do_circle(self, arg):
+            'Draw circle with given radius an options extent and steps:  CIRCLE 50'
+            print('Draw circle with given radius an options extent and steps:  CIRCLE 50')
+            
+        def do_position(self, arg):
+            'Print the current turtle position:  POSITION'
+            print('Print the current turtle position:  POSITION')
+            
+        def do_heading(self, arg):
+            'Print the current turtle heading in degrees:  HEADING'
+            print('Print the current turtle heading in degrees:  HEADING')
+            
+        def do_color(self, arg):
+            'Set the color:  COLOR BLUE'
+            print('Set the color:  COLOR BLUE')
+            
+        def do_undo(self, arg):
+            'Undo (repeatedly) the last turtle action(s):  UNDO'
+            print('Undo (repeatedly) the last turtle action(s):  UNDO')
 
-sys.exit(0)
+        def do_reset(self, arg):
+            'Clear the screen and return turtle to center:  RESET'
+            print('Clear the screen and return turtle to center:  RESET')
+
+        def do_bye(self, arg):
+            'Stop recording, close the turtle window, and exit:  BYE'
+            print('Thank you for using Turtle')
+            return True
+
+    TurtleShell().cmdloop()
 
 
-while p1.poll() is None and p2.poll() is None:
-    r, w, e = select.select([sys.stdin, master_fd1, master_fd2], [], [])
-    
-    if sys.stdin in r:
-        d = os.read(sys.stdin.fileno(), 10240)
-        os.write(master_fd1, d)
-        os.write(master_fd2, d)
-    
-    elif master_fd1 in r:
-        o = os.read(master_fd1, 10240)
-        if o:
-            os.write(sys.stdout.fileno(), o)
-
-    elif master_fd2 in r:
-        o = os.read(master_fd2, 10240)
-        if o:
-            os.write(sys.stdout.fileno(), o)
+test2()
   
