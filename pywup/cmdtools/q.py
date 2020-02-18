ec3362a474b6b1bd6afa5b4773eeae6e74d84691
Diego Souza <diego@wespa.com.br>
2020-02-18 16:18:13 -0300

    Adding beautify parameter (--b) to  wup q

--- pywup/cmdtools/q.py
+++ pywup/cmdtools/q.py
@@ -2,11 +2,13 @@ from pywup.services.system import error, Params
 
 import os
 
+
 def main(cmd, args):
     p = Params(cmd, args)
+
     p.map("query", 1, None, "SQL to be run")
     p.map("--q", 0, None, "Quiet mode. Do not output headers in the output")
-    p.map("--b", 0, None, "Enable beautify")
+    p.map("--b", 0, None, "Beautify output")
 
     if p.run():
         query = p.query
